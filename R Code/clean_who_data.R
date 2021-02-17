ec21a1092dc7d7a2a3bbe243498f19a2aeefb7cf
gilbermj <71649899+gilbermj@users.noreply.github.com>
2021-02-17 17:53:56 -0500

    Code for cleaning R data

---
+++ R Code/clean_who_data.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(here)
+
+mort10_1 <- read_csv(here('Data','who_mortality_data','Morticd10_part1','Morticd10_part1'))
+#mort10_2 <- read_csv("Grad School/2021-01 Spring/IA 530 - Econometrics-Ind Study/ia530_final_project/Data/who_mortality_data/Morticd10_part2/Morticd10_part2")
+
+#here()
