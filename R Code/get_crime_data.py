8a96acd0027acbb87e33aadee69076520d6a6ff1
gilbermj <71649899+gilbermj@users.noreply.github.com>
2021-03-08 13:37:31 -0500

    Updated data

---
+++ R Code/get_crime_data.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 13:11:02 2021
+
+@author: gilberm
+"""
+
+https://api.usa.gov/crime/fbi/sapi/api/data/arrest/national/offense/monthly/1999/2019?API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv
