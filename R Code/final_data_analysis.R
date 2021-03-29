c7ff867fcdbeca18531da772e8f44dd24a41bef9
gilbermj <71649899+gilbermj@users.noreply.github.com>
2021-03-28 23:15:20 -0400

    Updated Code and plots

--- R Code/final_data_analysis.R
+++ R Code/final_data_analysis.R
@@ -6,6 +6,8 @@ library(gridExtra)
 library(scales)
 library(dynlm)
 library(fastDummies)
+library(urca)
+library(vars)
 
 final_data <- read_csv(here('Data','final_data.csv'))
 
@@ -164,103 +166,152 @@ names(final_data_2)[16:27] <- gsub('month_name_','',names(final_data_2)[16:27])
 
 ts_final_data <- ts(final_data_2, start=c(1999,1), frequency=12)
 
-x24under_sa <- lm(x0_24_suicides_per_thous ~ Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov, data=ts_final_data)
-summary(x24under_sa)
+x24under_suicide_lm <- lm(x0_24_suicides_per_thous ~ Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov, data=ts_final_data)
+summary(x24under_suicide_lm)
 
-x24under_sa <- lm(x0_24_suicides_per_thous ~ Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov+Dec, data=ts_final_data)
-summary(x24under_sa)
+x25over_suicide_lm <- lm(x25on_suicides_per_thous ~ Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov, data=ts_final_data)
+summary(x25over_suicide_lm)
 
-x24under_sa <- lm(x0_24_suicides_per_thous ~ Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov+Dec, data=ts_final_data)
-summary(x24under_sa)
+x24under_menthlth_lm <- lm(ment_health_no_good_18_24 ~ Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov, data=ts_final_data)
+summary(x24under_menthlth_lm)
 
-x24under_sa <- lm(x0_24_suicides_per_thous ~ Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov+Dec, data=ts_final_data)
-summary(x24under_sa)
-
-vehicle_sa <- decompose(mydata[,"TOTALNSA"]) ### Decompose monthly oil into various components
-vehicle_sa <- vehicle_sa$x - vehicle_sa$seasonal  ## Subtract seasonal and trend
+x25over_menthlth_lm <- lm(ment_health_no_good_25on ~ Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov, data=ts_final_data)
+summary(x25over_menthlth_lm)
 
 
+# Remove seasonal component from suicide variables
 
+x24under_suicide_decomp <- decompose(ts_final_data[,'x0_24_suicides_per_thous'])
+x24under_suicide_sa <- x24under_suicide_decomp$x - x24under_suicide_decomp$seasonal  
 
+x25over_suicide_decomp <- decompose(ts_final_data[,'x25on_suicides_per_thous'])
+x25over_suicide_sa <- x25over_suicide_decomp$x - x25over_suicide_decomp$seasonal  
 
+# Remove seasonal component from mental health variables
 
+x24under_ment_hlth_decomp <- decompose(ts_final_data[,'ment_health_no_good_18_24'])
+x24under_ment_hlth_sa <- x24under_ment_hlth_decomp$x - x24under_ment_hlth_decomp$seasonal  
 
+x25over_ment_hlth_decomp <- decompose(ts_final_data[,'ment_health_no_good_25on'])
+x25over_ment_hlth_sa <- x25over_ment_hlth_decomp$x - x25over_ment_hlth_decomp$seasonal
+
+final_data_3 <- bind_cols(final_data_2, 
+                             x24under_suicide_sa=x24under_suicide_sa, 
+                             x25over_suicide_sa=x25over_suicide_sa, 
+                             x24under_ment_hlth_sa=x24under_ment_hlth_sa, 
+                             x25over_ment_hlth_sa=x25over_ment_hlth_sa)
+
+ts_final_data <- ts(final_data_3, start=c(1999,1), frequency=12)
 
 ############################ ADF Tests For Levels ##############################
 
-adfteststat_df <-ur.df(Data[,"WTISPLC"], type=c("trend"), selectlags="BIC") #summary(adfteststat)
-adfteststat_pp <-ur.pp(Data[,"WTISPLC"], type=("Z-tau"), model=c("trend"))
-adfteststat_adfgls <-ur.ers(Data[,"WTISPLC"], type="DF-GLS", model="trend")
-adfteststat_kpss <-ur.kpss(Data[,"WTISPLC"], type=c("tau"))
+stationarity_vars <- c('genhlth_no_good_18_24', 
+                       'genhlth_no_good_25on', 
+                       'divorced_widowed_separated', 
+                       'avg_close_apple_stock', 
+                       'avg_close_atandt_stock', 
+                       'avg_close_verizon_stock',
+                       'unemp_per',
+                       'personal_save_rate',
+                       'x24under_suicide_sa',
+                       'x25over_suicide_sa',
+                       'x24under_ment_hlth_sa',
+                       'x25over_ment_hlth_sa')
 
-stats <- cbind(adfteststat_df@teststat[1],adfteststat_pp@teststat[1],adfteststat_adfgls@teststat[1], adfteststat_kpss@teststat[1])
-cv <- cbind(adfteststat_df@cval[2],adfteststat_pp@cval[2], adfteststat_adfgls@cval[2], adfteststat_kpss@cval[2])
-stats <- rbind(stats,cv)
-rownames(stats) <- c("Test Statistic", "Critical Value")
-colnames(stats) <- c("ADF", "PP", "ADF-GLS", "KPSS")
+df_testStat <- c()
+df_critValue <- c()
+pp_testStat <- c()
+pp_critValue <- c()
+adfgls_testStat <- c()
+adfgls_critValue <- c()
+kpss_testStat <- c()
+kpss_critValue <- c()
+ 
 
-oil.fd <- diff(Data[,2]) # First Difference
-oil.hp <- mFilter(Data[,2],filter="HP")  # Hodrick-Prescott filter
-oil.bk <- mFilter(Data[,2],filter="BK")  # Baxter-King filter
-oil.cf <- mFilter(Data[,2],filter="CF")  # Christiano-Fitzgerald filter
+for(v in stationarity_vars){
+  temp_df <-ur.df(ts_final_data[,v], type=c("trend"), selectlags="BIC")
+  df_testStat <- c(df_testStat, temp_df@teststat[1])
+  df_critValue <- c(df_critValue, temp_df@cval[1,2])
+  
+  temp_pp <-ur.pp(ts_final_data[,v], type=("Z-tau"), model=c("trend"))
+  pp_testStat <- c(pp_testStat, temp_pp@teststat[1])
+  pp_critValue <- c(pp_critValue, temp_pp@cval[1,2])
+  
+  temp_adfgls <- ur.ers(ts_final_data[,v], type="DF-GLS", model="trend")
+  adfgls_testStat <- c(adfgls_testStat, temp_adfgls@teststat[1])
+  adfgls_critValue <- c(adfgls_critValue, temp_adfgls@cval[1,2])
+  
+  temp_kpss <- ur.kpss(ts_final_data[,v], type=c("tau"))
+  kpss_testStat <- c(kpss_testStat, temp_kpss@teststat[1])
+  kpss_critValue <- c(kpss_critValue, temp_kpss@cval[1,2])
+}
 
 
-gdp.fd <- diff(Data[,3]) # First Difference
-gdp.hp <- mFilter(Data[,3],filter="HP")  # Hodrick-Prescott filter
-gdp.bk <- mFilter(Data[,3],filter="BK")  # Baxter-King filter
-gdp.cf <- mFilter(Data[,3],filter="CF")  # Christiano-Fitzgerald filter
+stationarity_stats <- tibble(stationarity_vars,
+                             df_testStat,
+                             df_critValue,
+                             pp_testStat,
+                             pp_critValue,
+                             adfgls_testStat,
+                             adfgls_critValue,
+                             kpss_testStat,
+                             kpss_critValue)
 
-plot(oil.fd, main="First Difference of Crude Oil Prices", ylab="", xlab="Years")
-plot(gdp.fd, main="First Difference of Real GDP", ylab="", xlab="Years")
+stationarity_stats <- stationarity_stats %>%
+  mutate(df_result = if_else(abs(df_testStat)>abs(df_critValue),1,0),
+         pp_result = if_else(abs(pp_testStat)>abs(pp_critValue),1,0),
+         adfgls_result = if_else(abs(adfgls_testStat)>abs(adfgls_critValue),1,0),
+         kpss_result = if_else(abs(kpss_testStat)>abs(kpss_critValue),0,1),
+         total_stationary=df_result+pp_result+adfgls_result+kpss_result)
 
-plot(oil.hp$cycle, main="HP-Filtered Crude Oil Prices", ylab="", xlab="Years")
-plot(gdp.hp$cycle, main="HP-Filtered Real GDP", ylab="", xlab="Years")
 
-plot(oil.bk$cycle, main="BK-Filtered Crude Oil Prices", ylab="", xlab="Years")
-plot(gdp.bk$cycle, main="BK-Filtered Real GDP", ylab="", xlab="Years")
+nvar <- nrow(stationarity_stats)
+nobs <- nrow(ts_final_data)
 
-plot(oil.cf$cycle, main="CF-Filtered Crude Oil Prices", ylab="", xlab="Years")
-plot(gdp.cf$cycle, main="CF-Filtered Real GDP", ylab="", xlab="Years")
+final_data_4 <- final_data_3 %>%
+  select(month_end) %>%
+  filter(month_end!=ymd('1999-01-31'))
 
-### Finite Distributed Lag Models
-FDL <- dynlm(UNRATE ~ L(oil, 0:4) + L(FEDFUNDS, 0:4), data = Data)
-summary(FDL)
+for(i in 1:nvar){
+  
+  if(stationarity_stats[i,'total_stationary']==4){
+    var_name <- stationarity_vars[i]
+    
+    final_data_4 <- final_data_4 %>% 
+      mutate(!!var_name:=ts_final_data[2:nobs,stationarity_vars[i]])
+  } else {
+    var_name <- paste(stationarity_vars[i],'_noTrend',sep='')
+    final_data_4 <- final_data_4 %>% 
+      mutate(!!var_name:=diff(ts_final_data[,stationarity_vars[i]]))
+  }
+}
 
-### Autoregressive Distributed Lag Models
-ARDL <- dynlm(UNRATE ~ L(UNRATE, 1:4)  + L(oil, 0:4) + L(FEDFUNDS, 0:4), data = Data)
-summary(model_1)
+data_longer <- final_data_4 %>%
+  pivot_longer(cols=-month_end, names_to='variable', values_to='values')
 
-x24_under <- final_data_2 %>%
-  select(ment_health_no_good_18_24,
-         genhlth_no_good_18_24,
-         divorced_widowed_separated,
-         avg_close_apple_stock,
-         avg_close_atandt_stock,
-         avg_close_verizon_stock,
-         unemp_per,
-         personal_save_rate,
-         x0_24_suicides_per_thous) %>%
+ggplot(data_longer, aes(x=month_end,y=values)) +
+  geom_line() +
+  facet_wrap(~variable,scales="free")
+
+x24_under <- final_data_4 %>%
+  dplyr::select(-c(genhlth_no_good_25on_noTrend,
+            x25over_suicide_sa_noTrend,
+            x25over_ment_hlth_sa_noTrend,
+            x24under_suicide_sa_noTrend)) %>%
+  mutate(x24under_suicide_sa_noTrend=final_data_4$x24under_suicide_sa_noTrend) %>%
+  ts()
+
+fit <- VAR(x24_under, lag.max = 4, ic = "AIC")
+summary(fit)
+
+x25_over <- final_data_4 %>%
+  dplyr::select(-c(genhlth_no_good_18_24_noTrend,
+                   x25over_suicide_sa_noTrend,
+                   x25over_ment_hlth_sa_noTrend,
+                   x24under_suicide_sa_noTrend)) %>%
+  mutate(x24under_suicide_sa_noTrend=final_data_4$x24under_suicide_sa_noTrend) %>%
   ts()
 
 fit <- VAR(x24_under, lag.max = 12, ic = "AIC")
 summary(fit)
 
-x25_over <- final_data_2 %>%
-  select(ment_health_no_good_25on,
-         genhlth_no_good_25on,
-         divorced_widowed_separated,
-         avg_close_apple_stock,
-         avg_close_atandt_stock,
-         avg_close_verizon_stock,
-         unemp_per,
-         personal_save_rate,
-         x25on_suicides_per_thous) %>%
-  ts()
-
-fit <- VAR(x25_over, lag.max = 12, ic = "AIC")
-summary(fit)
-
-res_output_y <- irf(fit,impulse=c("y"), response=c("y"), n.ahead=12, cumulative = TRUE,runs=1000, ci=0.95)
-res_output_w <- irf(fit,impulse=c("w"), response="y", n.ahead=12, cumulative = TRUE,runs=100, ci=0.95)
-res_oil_w <- irf(fit,impulse=c("w"), response=c("w"), n.ahead=12, cumulative = TRUE,runs=1000, ci=0.95)
-res_oil_y <- irf(fit,impulse=c("y"), response="w", n.ahead=12, cumulative = TRUE,runs=100, ci=0.95)
